9f02b774717919ce172309b8c4dd347e09adecb7
Stefano BEZABEH MEUCCI <stefanomeucci@hotmail.com>
2018-10-23 08:12:24 +0200

    Modifcation Child server

--- src/HTTPServeur/ChildServeur.java
+++ src/HTTPServeur/ChildServeur.java
@@ -3,11 +3,14 @@ package HTTPServeur;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class ChildServeur implements Runnable {
     private Socket sock;
-    private OutputStream writer = null;
+    private PrintWriter writer = null;
     private BufferedInputStream reader = null;
 
     public ChildServeur(Socket sock) {
@@ -23,8 +26,40 @@ public class ChildServeur implements Runnable {
         while(!sock.isClosed()){
 
             try {
-                writer  = sock.getOutputStream();
+                writer  = new PrintWriter(sock.getOutputStream());
                 reader = new BufferedInputStream(sock.getInputStream())
+                //On attend la demande du client
+                String reponse = read();
+                InetSocketAddress remote = (InetSocketAddress)sock.getRemoteSocketAddress();
+
+                //On affiche quelques infos, pour le débuggage
+                String debug = "";
+                debug = "Thread : " + Thread.currentThread().getName() + ". ";
+                debug += "Demande de l'adresse : " + remote.getAddress().getHostAddress() +".";
+                debug += " Sur le port : " + remote.getPort() + ".\n";
+                debug += "\t -> Commande reçue : " + reponse + "\n";
+                System.err.println("\n" + debug);
+
+                //On traite la demande du client en fonction de la commande envoyée
+                String toSend = "";
+
+                //Traitement des donnée reçut
+                writer.write(toSend);
+                //Il FAUT IMPERATIVEMENT UTILISER flush()
+                //Sinon les données ne seront pas transmises au client
+                //et il attendra indéfiniment
+                writer.flush();
+                
+                if(closeConnexion){
+                    System.err.println("COMMANDE CLOSE DETECTEE ! ");
+                    writer = null;
+                    reader = null;
+                    sock.close();
+                    break;
+                }
+            }catch(SocketException e){
+                System.err.println("LA CONNEXION A ETE INTERROMPUE ! ");
+                break;
             } catch (IOException e) {
                 e.printStackTrace();
             }
